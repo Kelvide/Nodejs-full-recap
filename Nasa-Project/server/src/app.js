177fe32d824a41aaf7a15a0f5db9f7a328aa6e31
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-02-05 16:37:28 -0800

    connected the client and the server side together and added morgan for logging apis

--- Nasa-Project/server/src/app.js
+++ Nasa-Project/server/src/app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets.router')
 
 const app = express()
 
 app.use(cors())
+app.use(morgan('combined'))
+
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// Routes
 app.use(planetsRouter)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app
