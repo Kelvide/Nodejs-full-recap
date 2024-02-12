0c44f2029ff032f15ea0ff7d8cbc59b09ecb806b
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-02-12 00:27:28 -0800

    use clustering to fix event loop blocking function

--- NodeJs-Optimization/server.js
+++ NodeJs-Optimization/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cluster = require('cluster')
 
 const app = express()
+
+// Response time should be below 200ms or 100ms
 
 const delay = (duration) => {
     const startTime = Date.now();
@@ -10,13 +13,30 @@ const delay = (duration) => {
 }
 
 app.get('/', (req, res) => {
-    res.send('Performance example')
+    // A real example of event loop block function in NodeJs cause NodeJs is a single threaded language
+
+    // JSON.stringify({}) => "{}"
+    // JSON.parse("{}") => {}
+    // [5,4,3,2,1].sort()
+
+    // Key derivation functions to hash password that work as an event loop blocker
+    // crypto.scrypt()
+    // crypto.pbkdf2()
+    res.send(`Performance example ${process.pid}`)
 })
 
 app.get('/timer', (req, res) => {
     // Delaay the response
     delay(9000)
-    res.send('Ding ding ding')
+    res.send(`Ding ding ding ${process.pid}`)
 })
 
-app.listen(3000)
+if (cluster.isMaster) {
+    console.log("Master has been started")
+    // To create workers
+    cluster.fork();
+    cluster.fork();
+} else {
+    console.log("worker process started");
+    app.listen(3000)
+}
