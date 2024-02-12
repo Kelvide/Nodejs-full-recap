e12d6902b2defbfa1e9bae0f86f611a2b95b64f5
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-02-12 01:06:38 -0800

    checking out the pm2 for managing our clusters
    PM2- Process Manager 2
    
    pm2 start server.js
    pm2 stop 0 or server
    pm2 list or status
    pm2 delete 0 or server
    pm2 start server -i 2 or max(number of workers)
    pm2 logs to get logs
    pm2 logs --lines 200 to get logs 200 lines
    pm2 restart server
    pm2 start server.js -l logs.txt -i max
    pm2 show 0
    pm2 monit

--- NodeJs-Optimization/server.js
+++ NodeJs-Optimization/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cluster = require('cluster')
 
 const app = express()
 
 // Response time should be below 200ms or 100ms
-
 const delay = (duration) => {
     const startTime = Date.now();
     while (Date.now() - startTime < duration) {
@@ -13,30 +11,15 @@ const delay = (duration) => {
 }
 
 app.get('/', (req, res) => {
-    // A real example of event loop block function in NodeJs cause NodeJs is a single threaded language
-
-    // JSON.stringify({}) => "{}"
-    // JSON.parse("{}") => {}
-    // [5,4,3,2,1].sort()
-
-    // Key derivation functions to hash password that work as an event loop blocker
-    // crypto.scrypt()
-    // crypto.pbkdf2()
     res.send(`Performance example ${process.pid}`)
 })
 
 app.get('/timer', (req, res) => {
     // Delaay the response
     delay(9000)
-    res.send(`Ding ding ding ${process.pid}`)
+    res.send(`Beep beep beep ${process.pid}`)
 })
 
-if (cluster.isMaster) {
-    console.log("Master has been started")
-    // To create workers
-    cluster.fork();
-    cluster.fork();
-} else {
-    console.log("worker process started");
-    app.listen(3000)
-}
+
+console.log("worker process started");
+app.listen(3000)
