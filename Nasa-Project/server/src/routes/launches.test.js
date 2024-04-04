3cd0495ac515340b0d2bc9bf0140d9be3c7a3c07
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-04-04 03:42:28 -0700

    fixed the test script for the project perfectly working

--- Nasa-Project/server/src/routes/launches.test.js
+++ Nasa-Project/server/src/routes/launches.test.js
@@ -20,18 +20,18 @@ describe('Launches API', () => {
         const completeLaunchData = {
             mission: "USS Enterprise",
             rocket: "NCC 1701-p",
-            target: "kepler-186 f",
+            target: "Kepler-62 f",
             launchDate: "January 4, 2028"
         }
         const launctDataWithoutDate = {
             mission: "USS Enterprise",
             rocket: "NCC 1701-p",
-            target: "kepler-186 f",
+            target: "Kepler-62 f",
         }
         const launctDataWithInvalidDate = {
             mission: "USS Enterprise",
             rocket: "NCC 1701-p",
-            target: "kepler-186 f",
+            target: "Kepler-62 f",
             launchDate: "root"
         }
         test('It should respond with 201 created', async () => {
